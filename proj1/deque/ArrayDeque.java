9f42c20ee1fa082eed85ab46e8e3bb8b1c88cd81
PhysLi <2238661900@qq.com>
2025-12-04 21:45:33 +0800

    proj1 checkpoint attempt1

---
+++ proj1/deque/ArrayDeque.java
@@ -0,0 +1,155 @@
+package deque;
+
+import java.util.Iterator;
+import java.util.Objects;
+
+public class ArrayDeque<T> implements Deque<T>, Iterable<T> {
+    private int size;
+    private int front;
+    private int back;
+    private T[] items;
+
+    private class ArrayDequeIterator implements Iterator<T> {
+        private int Pos;
+
+        public ArrayDequeIterator() {
+            Pos = 0;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return Pos != size;
+        }
+
+        @Override
+        public T next() {
+            Pos++;
+            return get(Pos - 1);
+        }
+    }
+
+    public ArrayDeque() {
+        size = 0;
+        items = (T[]) new Object[8];
+        front = 0;
+        back = 0;
+    }
+
+    private int getIndex(int num) {
+        return num - items.length * Math.floorDiv(num, items.length);
+    }
+
+    private boolean getCase() {
+        return getIndex(back) >= getIndex(front);//两侧都有时为True
+    }
+
+
+    private int getBackSize() {
+        if (getCase()) {
+            return items.length - 1 - getIndex(back) ;
+        } else {
+            return 0;
+        }
+    }
+
+    private void resize(int capacity) {
+        T[] newArray = (T[]) new Object[capacity];
+        if (getCase()) {
+            System.arraycopy(items, getIndex(back + 1), newArray, 0, getBackSize());
+            System.arraycopy(items, 0, newArray, getBackSize(), front);
+        } else {
+            System.arraycopy(items, getIndex(back + 1), newArray, 0, size);
+        }
+        front = size;
+        back = -1;
+        items = newArray;
+    }
+
+    @Override
+    public void addFirst(T item) {
+        if (size == items.length - 1) {
+            resize(2 * size);
+        }
+        items[getIndex(back)] = item;
+        size++;
+        back--;
+        if (size == 1) {
+            front++;
+        }
+    }
+
+    @Override
+    public void addLast(T item) {
+        if (size == items.length - 1) {
+            resize(2 * size);
+        }
+        items[front] = item;
+        size++;
+        front++;
+        if (size == 1) {
+            back--;
+        }
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public void printDeque() {
+        for (int i = 0; i < size; i++) {
+            System.out.print(get(i) + " ");
+        }
+        System.out.println();
+    }
+
+    @Override
+    public T removeFirst() {
+        if (size == 0) {
+            return null;
+        }
+        back++;
+        size--;
+        T removed = items[getIndex(back)];
+        if (items.length >= 16 & (double) size / items.length < 0.25) {
+            resize(2 * size);
+        }
+        if (size == 0) {
+            front--;
+        }
+        return removed;
+    }
+
+    @Override
+    public T removeLast() {
+        if (size == 0) {
+            return null;
+        }
+        front--;
+        size--;
+        T removed = items[getIndex(front)];
+        if (items.length >= 16 & (double) size / items.length < 0.25) {
+            resize(2 * size);
+        }
+        if (size == 0) {
+            back++;
+        }
+        return removed;
+    }
+
+    @Override
+    public T get(int index) {
+        return items[getIndex(back + 1 + index)];
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new ArrayDequeIterator();
+    }
+}
---
+++ proj1/deque/LinkedListDeque.java
@@ -0,0 +1,165 @@
+package deque;
+
+import java.util.Iterator;
+import java.util.Objects;
+
+public class LinkedListDeque<T> implements Deque<T>, Iterable<T> {
+    private class DequeNode {
+        public T Item;
+        public DequeNode last;
+        public DequeNode next;
+        public DequeNode(T i) {
+            Item = i;
+        }
+        public DequeNode(T i, DequeNode l, DequeNode n) {
+            Item = i;
+            last = l;
+            next = n;
+        }
+
+        public void setNL(DequeNode l, DequeNode n) {
+            last = l;
+            next = n;
+        }
+    }
+
+    private class LLDequeIterator implements Iterator<T> {
+        private int Pos;
+        public LLDequeIterator() {
+            Pos = 0;
+        }
+        @Override
+        public boolean hasNext() {
+            return Pos != size;
+        }
+
+        @Override
+        public T next() {
+            Pos++;
+            return get(Pos - 1);
+        }
+    }
+
+    private DequeNode Sentinel;
+    private int size;
+
+    public LinkedListDeque() {
+        Sentinel = new DequeNode(null);
+        Sentinel.setNL(Sentinel, Sentinel);
+        size = 0;
+    }
+
+    @Override
+    public void addFirst(T item) {
+        DequeNode NewNode = new DequeNode(item, Sentinel, Sentinel.next);
+        Sentinel.next.last = NewNode;
+        Sentinel.next = NewNode;
+        size++;
+    }
+
+    @Override
+    public void addLast(T item) {
+        DequeNode NewNode = new DequeNode(item, Sentinel.last, Sentinel);
+        Sentinel.last.next = NewNode;
+        Sentinel.last = NewNode;
+        size++;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public void printDeque() {
+        DequeNode CurrentNode = Sentinel.next;
+        while (CurrentNode.next != Sentinel) {
+            System.out.print(CurrentNode.Item + " ");
+            CurrentNode = CurrentNode.next;
+        }
+        System.out.println();
+    }
+
+    @Override
+    public T removeFirst() {
+        T removed = Sentinel.next.Item;
+        Sentinel.next = Sentinel.next.next;
+        Sentinel.next.last = Sentinel;
+        if (size != 0) {
+            size--;
+        }
+        return removed;
+    }
+
+    @Override
+    public T removeLast() {
+        T removed = Sentinel.last.Item;
+        Sentinel.last = Sentinel.last.last;
+        Sentinel.last.next = Sentinel;
+        if (size != 0) {
+            size--;
+        }
+        return removed;
+    }
+
+    @Override
+    public T get(int index) {
+        DequeNode CurrentNode = Sentinel;
+        if (index > size) {
+            return null;
+        } else if (index < size / 2) {
+            int Pos = 0;
+            while (Pos != index) {
+                Pos++;
+                CurrentNode = CurrentNode.next;
+            }
+        } else if (index >= size / 2) {
+            int Pos = size + 1;
+            while (Pos != index) {
+                Pos--;
+                CurrentNode = CurrentNode.last;
+            }
+        }
+        return CurrentNode.Item;
+    }
+    private T getLLRecursive(int index, DequeNode current) {
+        if (index == 0) {
+            return current.Item;
+        } else {
+            return getLLRecursive(index--, current.next);
+        }
+    }
+
+    public T getRecursive(int index) {
+        return getLLRecursive(index, Sentinel);
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new LLDequeIterator();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof LinkedListDeque x) {
+            if (x.size() != size) {
+                return false;
+            } else {
+                int Pos = 0;
+                for (T i : this) {
+                    Pos++;
+                    if (i != x.get(Pos)) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+}
